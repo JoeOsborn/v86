bc919be6b08a26c155ea0e1ad841b46f417176c2
Joseph C. Osborn <joseph.osborn@pomona.edu>
2026-06-04 16:38:54 +0000

    fix graphical vga mode scaling on loading states

--- src/browser/screen.js
+++ src/browser/screen.js
@@ -336,7 +336,7 @@ export function ScreenAdapter(options, screen_fill_buffer)
 
         // initialize display mode and size to 80x25 text with 9x16 font
         this.set_mode(false);
-        this.set_size_text(80, 25);
+        this.set_size_text(80, 25, true);
         if(mode === MODE_GRAPHICAL_TEXT)
         {
             this.set_size_graphical(720, 400, 720, 400);
@@ -544,7 +544,7 @@ export function ScreenAdapter(options, screen_fill_buffer)
                 changed_rows.fill(1);
                 if(size_changed)
                 {
-                    this.set_size_graphical_text();
+                    this.set_size_graphical_text(true);
                 }
             }
         }
@@ -566,7 +566,10 @@ export function ScreenAdapter(options, screen_fill_buffer)
         graphic_context.fillRect(0, 0, graphic_screen.width, graphic_screen.height);
     };
 
-    this.set_size_graphical_text = function()
+    /**
+     * @param {boolean} force
+     */
+    this.set_size_graphical_text = function(force)
     {
         if(!font_context)
         {
@@ -577,7 +580,7 @@ export function ScreenAdapter(options, screen_fill_buffer)
         const gfx_height = font_height * text_mode_height;
         const offscreen_extra_height = font_height * 2;
 
-        if(!offscreen_context || offscreen_context.canvas.width !== gfx_width ||
+        if(force || !offscreen_context || offscreen_context.canvas.width !== gfx_width ||
             offscreen_context.canvas.height !== gfx_height ||
             offscreen_extra_context.canvas.height !== offscreen_extra_height)
         {
@@ -607,10 +610,11 @@ export function ScreenAdapter(options, screen_fill_buffer)
     /**
      * @param {number} cols
      * @param {number} rows
+     * @param {boolean} force
      */
-    this.set_size_text = function(cols, rows)
+    this.set_size_text = function(cols, rows, force)
     {
-        if(cols === text_mode_width && rows === text_mode_height)
+        if(!force && cols === text_mode_width && rows === text_mode_height)
         {
             return;
         }
@@ -642,7 +646,7 @@ export function ScreenAdapter(options, screen_fill_buffer)
         }
         else if(mode === MODE_GRAPHICAL_TEXT)
         {
-            this.set_size_graphical_text();
+            this.set_size_graphical_text(force);
         }
     };
 
@@ -670,7 +674,7 @@ export function ScreenAdapter(options, screen_fill_buffer)
             width * 2 < window.innerWidth * window.devicePixelRatio &&
             height * 2 < window.innerHeight * window.devicePixelRatio)
         {
-            base_scale = 1;
+            base_scale = 2;
         }
         else
         {
@@ -679,10 +683,6 @@ export function ScreenAdapter(options, screen_fill_buffer)
 
         update_scale_graphic();
     };
-
-    this.refresh_scale = function() {
-        this.set_scale(scale_x, scale_y);
-    }
 
     this.set_scale = function(s_x, s_y)
     {
--- src/browser/starter.js
+++ src/browser/starter.js
@@ -872,10 +872,6 @@ V86.prototype.restore_state = async function(state)
 {
     dbg_assert(arguments.length === 1);
     this.v86.restore_state(state);
-    if(this.screen_adapter)
-    {
-        this.screen_adapter.refresh_scale();
-    }
 };
 
 /**
